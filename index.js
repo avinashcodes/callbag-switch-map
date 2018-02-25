8cc368e645325d7e75d50a0c17e84070291ae383
avinashsmiles4444 <avinashsmiles4444@hotmail.com>
2018-02-25 16:05:11 +0530

    Added code, test and package.json

---
+++ index.js
@@ -0,0 +1,31 @@
+const switchMap = (makeSource, combineResults) => inputSource => (start, outputSink) => {
+	if(start !== 0) return;
+	if(!combineResults) combineResults = (x, y) => y;
+	let currSourceTalkback = null;
+
+	const unsubscribeCurrSource = function(){
+		if(currSourceTalkback){
+			currSourceTalkback(2);
+		}
+	};
+	
+	inputSource(0, (t,d) => {
+		if(t === 1){
+			unsubscribeCurrSource();
+			
+			let currSource = makeSource(d);
+
+			currSource(0, (currT, currD) => {
+				if(currT === 0) currSourceTalkback = currD;
+				if(currT === 1) outputSink(t, combineResults(d, currD));
+			});
+		} else {
+			if(t === 2){
+				unsubscribeCurrSource();
+			}
+			outputSink(t, d);
+		}
+	});
+}
+
+module.exports = switchMap;
