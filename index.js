bfe4cd9267eaf799ce6050ff7f282301e2162764
avinashsmiles4444 <{ID}+{username}@users.noreply.github.com>
2018-03-20 21:50:47 +0530

    Wait for curr source before terminating

--- index.js
+++ index.js
@@ -3,29 +3,31 @@ const switchMap = (makeSource, combineResults) => inputSource => (start, outputS
 	if(!combineResults) combineResults = (x, y) => y;
 	let currSourceTalkback = null;
 
-	const unsubscribeCurrSource = function(){
-		if(currSourceTalkback){
-			currSourceTalkback(2);
-		}
-	};
-	
 	inputSource(0, (t,d) => {
+		if(t === 0) outputSink(t, d); 
 		if(t === 1){
-			unsubscribeCurrSource();
+			if(currSourceTalkback){
+				currSourceTalkback(2);
+				currSourceTalkback = null;
+			}
 			
 			let currSource = makeSource(d);
 
 			currSource(0, (currT, currD) => {
 				if(currT === 0) currSourceTalkback = currD;
 				if(currT === 1) outputSink(t, combineResults(d, currD));
-				if(currT === 0 || currT === 1) currSourceTalkback(1);
+				if(currT === 0 || currT === 1) currSourceTalkback && currSourceTalkback(1);
+				if(currT === 2){
+					currSourceTalkback = null;
+					if(sourceEnded) outputSink(currT, currD);
+				}
 			});
-		} else {
-			if(t === 2){
-				unsubscribeCurrSource();
-			}
-			outputSink(t, d);
+		} 
+		if(t === 2){
+			sourceEnded = true;
+			if(!currSourceTalkback) outputSink(t, d);
 		}
+		
 	});
 }
 
